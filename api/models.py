8536adf7b4b2bf348933c715ee1bdc29c15016c4
Oluchi Onyekwusi <meachelleoluchi@gmail.com>
2026-07-17 23:06:43 +0100

    Add book model and serializers with validation

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    author = models.CharField(max_length=255)     
+    isbn = models.CharField(max_length=17, unique=True)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
--- api/views.py
+++ api/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Book
+from .serializer import BookSerializer
 
 # Create your views here.
+class BookView(APIView):
+    """ List all books, or create a new book """
+    def post(self, request, *args, **kwargs):
+        # Instantiate the serializer and pass in the request data sent by the client
+        serializer = BookSerializer(data=request.data)
+        # Check that the request is valid. e.g.: Was the title provided?, if not autoamtically return an error
+        serializer.is_valid(raise_exception=True)
+        # Save the new book using the serializer
+        serializer.save()
+        # Return the serialized JSON object of the created book
+        return Response(serializer.data)
+       
+    def get(self, request, *args, **kwargs):
+        # Get all books in the table
+        books = Book.objects.all()
+        # Serialize the list of books, specify `many=True` to tell the serializer we are passing in a list of books and not a single instance
+        serializer = BookSerializer(books, many=True)
+        # Return the serialized JSON
+        return Response(serializer.data)
+    
+book_view = BookView.as_view()
